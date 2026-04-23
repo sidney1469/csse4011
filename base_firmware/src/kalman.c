9301fd541f6ef56119b83a1440a24accb8841e25
sidney1469 <sidneyneil04@outlook.com>
2026-04-23 10:57:54 +1000

    added kalman type shit

--- base_firmware/src/kalman.c
+++ base_firmware/src/kalman.c
@@ -11,7 +11,7 @@ extern struct k_msgq kalman_data_msgq;
 
 K_MSGQ_DEFINE(kalman_data_msgq, sizeof(struct kalman_struct), 10, 4);
 
-int calculate_kalman(int8_t value[KALMAN_NUM_CHANNELS], float out[KALMAN_NUM_CHANNELS]) {
+int calculate_kalman(int8_t value, float out) {
 
     float Q = 0.022f;
     float R = 0.617f;
@@ -26,29 +26,24 @@ int calculate_kalman(int8_t value[KALMAN_NUM_CHANNELS], float out[KALMAN_NUM_CHA
         return -1;
     }
 
-    float last_value[13];
-    float P_last[13];
+    float last_value;
+    float P_last;
 
-    for (int i = 0; i < 13; i++) {
-        last_value[i] = (float)newdata.last_value[i];
-        P_last[i]     = (float)newdata.P_last[i];
-    }
+    last_value = (float)newdata.last_value;
+    P_last     = (float)newdata.P_last;
+    
 
-    float P_data[13];
+    float P_data;
 
-    for (int i = 0; i < 13; i++) {
-        P_temp  = P_last[i] + Q;
-        K       = P_temp * (1.0f / (P_temp + R));
-        x_est   = last_value[i] + K * ((float)value[i] - last_value[i]);
-        P_data[i] = (1.0f - K) * P_temp;
+    P_temp  = P_last + Q;
+    K       = P_temp * (1.0f / (P_temp + R));
+    x_est   = last_value + K * ((float)value - last_value);
+    P_data = (1.0f - K) * P_temp;
 
-        out[i] = (int8_t)x_est;
-    }
-
-    for (int i = 0; i < 13; i++) {
-        newdata.last_value[i] = out[i];
-        newdata.P_last[i]     = (int8_t)P_data[i];
-    }
+    out = (int8_t)x_est;
+    
+    newdata.last_value = out;
+    newdata.P_last     = (int8_t)P_data;
 
     if (k_msgq_put(&kalman_data_msgq, &newdata, K_NO_WAIT) != 0) {
         return -2;
--- base_firmware/src/kalman.h
+++ base_firmware/src/kalman.h
@@ -4,15 +4,13 @@
 #include <zephyr/kernel.h>
 #include <zephyr/types.h>
 
-#define KALMAN_NUM_CHANNELS 13
-
 struct kalman_struct {
-    int8_t last_value[KALMAN_NUM_CHANNELS];
-    int8_t P_last[KALMAN_NUM_CHANNELS];
+    int8_t last_value;
+    int8_t P_last;
 };
 
 extern struct k_msgq kalman_data_msgq;
 
-int calculate_kalman(int8_t value[KALMAN_NUM_CHANNELS], float out[KALMAN_NUM_CHANNELS]);
+int calculate_kalman(int8_t value, float out);
 
 #endif /* KALMAN_H */
